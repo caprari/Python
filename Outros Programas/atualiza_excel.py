e47c11626c88e10b93007fdfa221c4f42c03b73f
Marcelo de Carvalho Caprari <107228596+caprari@users.noreply.github.com>
2022-06-29 23:40:46 -0300

    novos comandos pandas

--- Outros Programas/atualiza_excel.py
+++ Outros Programas/atualiza_excel.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
-tabela = pd.read_excel('Produtos.xlsx')
+tabela = pd.read_excel('Produtos_pandas.xlsx')
 vendas_df = pd.DataFrame(tabela)
 
 # print(tabela) #print de toda a tabela
 # print(tabela.head(4)) #print dos 4 primeiros registros
 
-print(vendas_df) #toda a tabela
-print()
+# print(vendas_df) #toda a tabela
+# print()
 
 # print(vendas_df.head(4)) #exibir qtde de registros
 # print()
@@ -37,7 +37,52 @@ print()
 #                                          'Produto', ['Produtos', 'Preço Atualizado']]
 
 # print(vendas_servico_df_resumo)
+# atualizar um campo da coluna
+# tabela.loc[tabela['Tipo'] == 'Produto', 'Multiplicador'] = 1.4
+# tabela['Preço Atualizado'] = tabela['Multiplicador'] * tabela['Preço Base Original']
+# gravar novo arquivo excel
+# vendas_df.to_excel('Produtos_pandas.xlsx', index=False)
 
-tabela.loc[tabela['Tipo'] == 'Produto', 'Multiplicador'] = 1.4
-tabela['Preço Atualizado'] = tabela['Multiplicador'] * tabela['Preço Base Original']
+#criar nova coluna
+# vendas_df['Comissão'] = vendas_df['Preço Atualizado'] * 0.05
+# vendas_df.loc[:, 'Imposto'] = 1
+
+#excluir colunas
+# vendas_df = vendas_df.drop('Preço Atualizado', axis=1)
+# vendas_df = vendas_df.drop(0, axis=0)
+
+# deletar linhas completamente vazias
+# vendas_df = vendas_df.dropna(how='all', axis=1)
+
+# deletar linhas que possuem algum registro vazio
+# vendas_df = vendas_df.dropna()
+
+# preencher valores vazios com 1
+# vendas_df['Comissão'] = vendas_df['Comissão'].fillna(1)
+
+# preencher valores vazios com media
+# vendas_df['Comissão'] = vendas_df['Comissão'].fillna(vendas_df['Comissão'].mean())
+
+# preencher com o ultimo valor acima caso esteja vazio
+# vendas_df = vendas_df.ffill()
+
+# soma a quantidade de linhas conforme o parâmetro
+# tipo = vendas_df['Tipo'].value_counts()
+# print(tipo)
+
+# agrupar os valores conforme a coluna
+# faturamento = vendas_df.groupby('Tipo').sum()
+# print(faturamento)
+
+# agrupar os valores conforme a coluna e mostrar apenas algumas colunas
+# faturamento2 = vendas_df[['Tipo', 'Preço Base Original', 'Preço Atualizado']].groupby('Tipo').sum()
+# print(faturamento2)
+
+responsavel = pd.read_excel('Responsavel_pandas.xlsx')
+vendas_df = vendas_df.merge(responsavel)
+
+
+
+print(vendas_df)
 vendas_df.to_excel('Produtos_pandas.xlsx', index=False)
+
