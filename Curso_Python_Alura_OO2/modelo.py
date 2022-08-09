e2bc81b3ca047add7a9506bec9d052687003aa60
Marcelo de Carvalho Caprari <107228596+caprari@users.noreply.github.com>
2022-08-08 23:22:38 -0300

    atualizacao

--- Curso_Python_Alura_OO2/modelo.py
+++ Curso_Python_Alura_OO2/modelo.py
@@ -19,17 +19,31 @@ class Programa:
     def nome(self, novo_nome):
         self._nome = novo_nome.title()
 
+    # def imprime(self):
+    #     print(f'{self._nome} - {self.ano} - {self._likes} likes')
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self._likes} likes'
+
 
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
 
+    # def imprime(self):
+    #     print(f'{self._nome} - {self.ano} - {self.duracao} min - {self._likes} likes')
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self.duracao} min - {self._likes} likes'
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporada):
         super().__init__(nome, ano)
         self.temporada = temporada
+
+    # def imprime(self):
+    #     print(f'{self._nome} - {self.ano} - {self.temporada} temporadas - {self._likes} likes')
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self.temporada} temporadas - {self._likes} likes'
 
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
@@ -37,7 +51,7 @@ vingadores.dar_likes()
 # print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} '
 #       f'- Duração: {vingadores.duracao} minutos '
 #       f'- Likes: {vingadores.likes}')
-print(f'Nome: {vingadores.nome} - Likes: {vingadores.likes}')
+# print(f'Nome: {vingadores.nome} - Likes: {vingadores.likes}')
 # print(f'O filme {vingadores.nome} foi lançado em {vingadores.ano}'
 #       f' e tem duração de {vingadores.duracao} minutos.')
 
@@ -47,7 +61,19 @@ atlanta.dar_likes()
 # print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} '
 #       f'- Temporadas: {atlanta.temporada} '
 #       f'- Likes: {atlanta.likes}')
-print(f'Nome: {atlanta.nome} - Likes: {atlanta.likes}')
+# print(f'Nome: {atlanta.nome} - Likes: {atlanta.likes}')
+
+filmes_e_series = [vingadores, atlanta]
+
+for programa in filmes_e_series:
+    # detalhes = programa.duracao if hasattr(programa, 'duracao') else programa.temporada
+    # if hasattr(programa, 'duracao'):
+    #     detalhes = programa.duracao
+    # else:
+    #     detalhes = programa.temporada
+    # print(f'Nome: {programa.nome} - {detalhes} - Likes: {programa.likes}')
+    # programa.imprime()
+    print(programa)
 
 
 
